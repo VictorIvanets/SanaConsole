384840a11ce5ea04a9144bba66a0be847c99bc9d
Виктор <imperia.zt@gmail.com>
2024-10-24 18:04:11 +0300

    Add project files.

---
+++ SanaConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string output = "ENTER SCORE";
+        Console.WriteLine(output);
+
+        string score = Console.ReadLine();
+        double ageToInt;
+
+        if (!double.TryParse(score, out ageToInt))
+        {
+            Console.WriteLine($"ENTER double (0,0)!! NOT {score}");
+            return;
+        }
+   
+        if (ageToInt >= 90 && ageToInt <= 100) Console.WriteLine("SUPER");
+
+        else 
+        if (ageToInt >= 70 &&  ageToInt <= 89 ) Console.WriteLine("GOOD");   
+        
+        else Console.WriteLine("Not SUPER");
+               
+            
+
+
+    
+    }
+
+}
